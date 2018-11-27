25ed352f7a34252e9d32ffcf489dddfaf6e6b1d3
Amory Galili <amorygalili@gmail.com>
2018-11-27 01:38:33 -0500

    Created redux store for app which is used to maintain state. State includes hal data and layout information.

--- robotpy_websim/html/src/app.js
+++ robotpy_websim/html/src/app.js
@@ -1,9 +1,14 @@
+import store from "assets/js/store/index";
 import riot from 'riot';
+import riotReduxConnect from 'riot-redux-connect';
 import "assets/js/tags/app.tag";
 
 window.sim = {
-  riot
+  riot,
+  store
 };
+
+riotReduxConnect(riot, store);
 
 riot.mount('app');
 
---
+++ robotpy_websim/html/src/assets/js/reducers/index.js
@@ -0,0 +1,14 @@
+import * as ActionTypes from "../constants/action-types";
+
+const initialState = {
+  
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
