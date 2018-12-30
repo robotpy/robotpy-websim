cdaae37a7ed190a8b0f1740e626601a4861971f6
Amory Galili <amorygalili@gmail.com>
2018-12-30 01:05:48 -0500

    Removed old examples. Added new example for 4 wheel physics

---
+++ examples/physics-4wheel/src/sim/physics.js
@@ -0,0 +1,53 @@
+
+
+class MyUserPhysics extends UserPhysics {
+
+  createRobotModel(robotConfig) {
+    let math = this.Math;
+
+    let MOTOR_CFG_CIM = {
+      name: 'CIM', 
+      nominalVoltage: math.unit(12, 'volt'),
+      freeSpeed: math.unit(5310, 'cpm'),
+      freeCurrent: math.unit(2.7, 'A'),
+      stallTorque: math.unit(2.42, 'Nm'),
+      stallCurrent: math.unit(133, 'A')
+    };
+    
+    let bumperWidth = math.unit(3.25, 'inch');
+
+    let model = this.Models.TankModel.theory(
+      MOTOR_CFG_CIM,
+      math.unit(50, 'kg'),     
+      10.71,
+      2,
+      math.unit(22, 'inch'),
+      math.add(math.unit(23, 'inch'), math.multiply(bumperWidth, 2)),
+      math.add(math.unit(32, 'inch'), math.multiply(bumperWidth, 2)),
+      math.unit(6, 'inch')
+    );
+
+    return model;
+  }
+
+  updateSim(halData, dt) {
+
+    const dataOut = halData.out;
+    const pwm = dataOut.pwm;
+
+    let lrMotor = pwm[1].value;
+    let rrMotor = pwm[2].value;
+
+    // Not needed because front and rear should be in sync
+    // lfMotor = pwm[3].value;
+    // rfMotor = pwm[4].value;
+    
+    let {rcw, fwd} = this.model.getVector(lrMotor, rrMotor, dt);
+
+    let xSpeed = fwd * Math.cos(this.robot.angle);
+    let ySpeed = fwd * Math.sin(this.robot.angle);
+  
+    this.Matter.Body.setVelocity(this.robot, { x: xSpeed, y: ySpeed });
+    this.Matter.Body.setAngularVelocity(this.robot, rcw);
+  }
+}
